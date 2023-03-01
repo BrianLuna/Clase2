b94f45a8094790251062a3365dd00234a8e7fc4b
Brian <brian.damian.luna@gmail.com>
2023-03-01 19:15:05 -0300

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numeroInicio = 2;
+        int numeroFin = 12;
+        boolean pares = false;
+
+        while(numeroInicio <= numeroFin){
+            if(numeroInicio % 2 == 0 && pares){
+                System.out.println(numeroInicio);
+            }
+
+            if(numeroInicio % 2 == 1 && !pares){
+                System.out.println(numeroInicio);
+            }
+
+            numeroInicio++;
+        }
+    }
+}
