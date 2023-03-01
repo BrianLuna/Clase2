83e284aef0942c57f886a02714c4b5cc99d3dccd
Brian <brian.damian.luna@gmail.com>
2023-03-01 19:31:31 -0300

    Agregando nueva funcionalidad Hola

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
             if(numeroInicio % 2 == 1 && !pares){
                 System.out.println(numeroInicio + 1);
+                System.out.println("Hola");
             }
 
             numeroInicio++;
